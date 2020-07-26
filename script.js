24fa484946ac957ce6ce98cb28a14a01767efcf7
Huzaifa Ehsan <56621886+huzaifaehsan1@users.noreply.github.com>
2020-07-26 15:37:15 -0400

    Completed the extension

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ function replaceText(element) {
     element.childNodes.forEach(replaceText);
   } else if (element.nodeType == Text.TEXT_NODE) {
     if (
-      //element.textContent.match(/sex/gi) ||
-      element.textContent.match(/corona/gi)
+      element.textContent.match(/sex/gi) ||
+      element.textContent.match(/porn/gi) ||
+      element.textContent.match(/fuck/gi) ||
+      element.textContent.match(/piss/gi) ||
+      element.textContent.match(/dick/gi) ||
+      element.textContent.match(/asshole/gi) ||
+      element.textContent.match(/bitch/gi) ||
+      element.textContent.match(/bastard/gi)
     ) {
       element.parentElement.remove();
       alert("Haram");
